18bc2c50a0eaee55f88d3b24a855e88f365ecb49
Olijujuan Green <nihongojaydee@gmail.com>
2023-02-07 13:03:51 -0500

    models created

---
+++ data-models/Carpool.js
@@ -0,0 +1,14 @@
+class Carpool {
+    constructor(
+        id,
+        owner,
+        members
+    )
+    {
+        this.id = id
+        this.owner = owner
+        this.members = members
+    }
+}
+
+export { Carpool }

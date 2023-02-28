d8ad831220a3ae6061fb0de94998751132393c5d
Olijujuan Green <nihongojaydee@gmail.com>
2023-02-28 12:49:05 -0500

    Created carpool model

---
+++ web/src/models/Carpool.js
@@ -0,0 +1,11 @@
+import { Schema, model } from "mongoose";
+
+const CarpoolSchema = new Schema({
+  owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true },
+  members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  descrtiption: { type: String, required: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default model("Carpool", CarpoolSchema);

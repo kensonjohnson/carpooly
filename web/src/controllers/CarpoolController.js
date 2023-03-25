15b7f29cb0a34003a08e1578b5a9ea49ebe8fd95
Kenson Johnson <kenson.johnson@outlook.com>
2023-03-24 21:35:54 -0500

    Added create carpool page and functionality
    Created page and form to create carpools.
    
    Carpools are created with owner id and carpool id is saved on user info.

--- web/src/controllers/CarpoolController.js
+++ web/src/controllers/CarpoolController.js
@@ -1,3 +1,27 @@
-export function createCarpool(req, res, next) {
-  res.redirect("/dashboard");
+import User from "../models/User.js";
+import Carpool from "../models/Carpool.js";
+
+export async function createCarpool(req, res, next) {
+  const { name, description } = req.body;
+  const errors = [];
+  if (!name || !description) {
+    errors.push({ msg: " Please fill in all fields." });
+  }
+  if (errors.length) {
+    res.render("createCarpools", { errors, name, description });
+  }
+  try {
+    const user = await User.findOne({ _id: req.user._id });
+    if (user.carpoolsOwned.length >= 5) {
+      errors.push({ message: "You already have 5 carpools!" });
+      res.redirect("/dashboard");
+    }
+    const newCarpool = new Carpool({ owner: user._id, name, description });
+    await newCarpool.save();
+    user.carpoolsOwned.push(newCarpool._id);
+    await user.save();
+    res.redirect("/dashboard");
+  } catch (err) {
+    next(err);
+  }
 }

738c7abfcfa3f2f32ca4814ca48676e147bbc194
Kenson Johnson <kenson.johnson@outlook.com>
2023-04-26 14:23:30 -0500

    Implemented profile picture upload to S3
    Used express-fileupload to get the file blob from the browser, passed
    the data property into a buffer, and used that plus uuidv4 to
    upload the file to S3 bucket.
    
    Modified the profile.ejs to render the uploaded pic by pulling it from
    the Cloudfront distribution.
    
    Added null checking for the rare cases when the browser fails to
    upload the image and pass it on.

--- web/src/routes/dashboard.js
+++ web/src/routes/dashboard.js
@@ -4,9 +4,8 @@ import { ensureAuthenticated } from "../config/auth.js";
 import { createCarpool } from "../controllers/CarpoolController.js";
 import Carpool from "../models/Carpool.js";
 import User from "../models/User.js";
-// import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import AWS from "aws-sdk";
-// import { readFileSync } from "fs";
 
 router.get("/", ensureAuthenticated, async (req, res) => {
   try {
@@ -85,13 +84,13 @@ router.post(
 // Route to edit profile page
 router.get("/profile", ensureAuthenticated, (req, res) => {
   const { name, profilePicURI } = req.user;
-  // console.log("HERE", req.user);
   if (profilePicURI) {
     const data = { name, profilePicURI };
-    res.render("profile", { data });
+    return res.render("profile", { data });
   }
-
-  res.render("profile", { data: { name: "Default", profilePicURI: null } });
+  res.render("profile", {
+    data: { name: "Default", profilePicURI: req.user.profilePicURI },
+  });
 });
 
 // Route to upload profile pic
@@ -103,12 +102,41 @@ router.post(
     const s3 = new AWS.S3({
       accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+      region: "us-east-2",
     });
-    console.log(req.files);
-    // const blob = readFileSync(req.body.profilePic.files[0].path)
-    res.render("profile", {
-      data: { name: req.user.name, profilePicURI: req.user.profilePicURI },
-    });
+
+    // In rare cases, req.files can be null
+    if (req.files) {
+      const fileContent = Buffer.from(req.files.profilePic.data, "binary");
+
+      const uniqueName = uuidv4() + req.files.profilePic.name;
+
+      const params = {
+        Bucket: "carpooly",
+        Key: uniqueName, // File name you want to save as in S3
+        Body: fileContent, // The file buffer
+      };
+
+      s3.upload(params, (err) => {
+        if (err) {
+          next(err);
+        }
+      });
+
+      const cloudfrontURI = process.env.CLOUDFRONT_URI + uniqueName;
+
+      user.profilePicURI = cloudfrontURI;
+      await user.save();
+      req.user.profilePicURI = cloudfrontURI;
+
+      res.render("profile", {
+        data: { name: req.user.name, profilePicURI: cloudfrontURI },
+      });
+    } else {
+      res.render("profile", {
+        data: { name: req.user.name, profilePicURI: req.user.profilePicURI },
+      });
+    }
   }
 );
 
